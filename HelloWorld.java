2096ebce352d2ea5f8fc3b4ce2ec66a0e4df010c
sangampachauri <sangam.pachauree@gmail.com>
2024-06-18 09:20:39 +0530

    Take name of the user

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld{
     public static void main(String []args){
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scan.next();
+        System.out.println("Hi "+name+", This is Hello World Program");
+        scan.close();
     }
 }
